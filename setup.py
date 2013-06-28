a6975d9b3ba877acd88291c33eb4226ec1290433
Ben Cordero (bencord0) <bmc@linux.com>
2013-06-28 23:28:38 +0100

    Package a little bit better

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='{{ project_name }}',
+    packages=['{{ project_name }}'],
     entry_points={
         'console_scripts': [
             '{{ project_name }} = {{ project_name }}.wsgi:main'
