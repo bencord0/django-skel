7f79309689f9bd312cc8d8b7ac0b6ee548b95002
Ben Cordero (bencord0) <bmc@linux.com>
2013-06-29 22:05:06 +0100

    package the subpackages
    This lets commands such as "setup.py sdist" work properly

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(name='{{ project_name }}',
-    packages=['{{ project_name }}'],
+    packages=[
+        '{{ project_name }}',
+        '{{ project_name }}.apps',
+        '{{ project_name }}.libs',
+        '{{ project_name }}.settings',
+        ],
     entry_points={
         'console_scripts': [
             '{{ project_name }} = {{ project_name }}.wsgi:main'
-        ]
-    }
+            ]
+        }
 )
