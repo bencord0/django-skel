c9cdef7e37760f418280e837e7446d826c84fce5
Ben Cordero (bencord0) <bmc@linux.com>
2013-06-29 22:07:10 +0100

    Provide a sensible Out-Of-Box landing page

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
@@ -8,6 +9,8 @@ admin.autodiscover()
 
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
+    # Redirect / to admin
+    url(r'^$', RedirectView.as_view(url='/admin/')),
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
