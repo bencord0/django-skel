28310fe64f6e88787b3266be182ff2c1645a72ee
Ben Cordero (bencord0) <bmc@linux.com>
2013-08-04 17:30:43 +0100

    Find packages to distribute

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+os.chdir(os.path.dirname(os.path.abspath(os.path.normpath(__file__))))
 
 setup(name='{{ project_name }}',
-    packages=[
-        '{{ project_name }}',
-        '{{ project_name }}.apps',
-        '{{ project_name }}.libs',
-        '{{ project_name }}.settings',
-        ],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             '{{ project_name }} = {{ project_name }}.wsgi:main'
